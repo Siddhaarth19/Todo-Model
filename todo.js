f42e123518c760cadea76f202e6440899dc918a0
Siddhaarth19 <95361426+Siddhaarth19@users.noreply.github.com>
2022-11-14 20:33:17 +0530

    initial commit

---
+++ todo.js
@@ -0,0 +1,44 @@
+const todoList = () => {
+  all = [];
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
+
+  const overdue = () => {
+    return all.filter((item) => item.dueDate < today);
+  };
+
+  const dueToday = () => {
+    return all.filter((item) => item.dueDate === today);
+  };
+
+  const dueLater = () => {
+    return all.filter((item) => item.dueDate > today);
+  };
+
+  const toDisplayableList = (list) => {
+    let out = list
+      .map(
+        (item) =>
+          `${item.completed ? "[x] " : "[ ] "}${item.title} ${
+            item.dueDate === today ? " " : item.dueDate
+          }`
+      )
+      .join("\n");
+    return out;
+  };
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
+module.exports = todoList;
